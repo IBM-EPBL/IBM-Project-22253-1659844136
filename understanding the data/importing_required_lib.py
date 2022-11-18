0fba2605ac46872ad5606d985397dd6d32c9fe78
sasi200211 <81792622+sasi200211@users.noreply.github.com>
2022-11-18 10:51:03 +0530

    Update importing_required_lib.py

--- understanding the data/importing_required_lib.py
+++ understanding the data/importing_required_lib.py
@@ -1,6 +1,12 @@
-import tensorflow as tf
+import numpy   #used for numerical analysis
+import tensorflow as tf  #open source used for both ML and DL for computation
+from tensorflow.keras.datasets import mnist  #mnist dataset
+from tensorflow.keras.models import Sequential #it is a plain stack of Layers
+from tensorflow.keras import layers #A Layer consists of a tensor-in tensor-out computation function
+from tensorflow.keras.layers import Dense, Flatten #Dense-Dense Layer is the regular deeply connected n
+#Faltten-used fot flattening the input or change the dimension
+from tensorflow.keras.layers import Conv2D #Convolutional Layer
+from keras.optimizers import Adam #optimizer
+from keras.utils import np_utils #used for one-hot encoding
 import matplotlib.pyplot as plt
 import numpy as np
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense,Dropout,Activation,Flatten,Conv2D,MaxPooling2D
-import cv2
